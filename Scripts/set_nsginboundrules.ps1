f0d475d955cf576e046bb90e3cdb0b61b1d3a940
Brad Watts <bwatts@microsoft.com>
2019-05-14 13:20:16 -0400

    Added scripts along with outputing NSG Name

---
+++ Scripts/set_nsginboundrules.ps1
@@ -0,0 +1,22 @@
+﻿param(
+    [string]$nsgName,
+    [string]$ruleName,
+    [string]$ruleDescription,
+    [string]$access,
+    [string]$protocol,
+    [string]$direction,
+    [int]$priority,
+    [string]$sourceAddressPrefix,
+    [string]$sourcePortRange,
+    [string]$destinationAddressPrefix,
+    [string]$destinationPortRange = 3389
+)
+
+$nsg = Get-AzNetworkSecurityGroup -Name LABVM-nsg
+
+$nsg | Add-AzNetworkSecurityRuleConfig -Name $ruleName -Description $ruleDescription `
+    -Access $access -Protocol $protocol -Direction $direction -Priority $priority -SourceAddressPrefix $sourceAddressPrefix `
+    -SourcePortRange $sourcePortRange -DestinationAddressPrefix $destinationPortRange -DestinationPortRange $destinationPortRange
+
+
+$nsg | Set-AzNetworkSecurityGroup
