d62ddf0b0c70a2638240b1bcd86a113b84383c6d
Brad Watts <bwatts@microsoft.com>
2019-05-17 11:13:25 -0400

    Changed script to use new AZ module

--- Scripts/set_nsginboundrules.ps1
+++ Scripts/set_nsginboundrules.ps1
@@ -13,11 +13,11 @@
     [string]$rgName
 )
 
-$nsg = Get-AzureRMNetworkSecurityGroup -Name $nsgName -ResourceGroupName $rgName
+$nsg = Get-AzNetworkSecurityGroup -Name $nsgName -ResourceGroupName $rgName
 
-$nsg | Add-AzureRMNetworkSecurityRuleConfig -Name $ruleName -Description $ruleDescription `
+$nsg | Add-AzNetworkSecurityRuleConfig -Name $ruleName -Description $ruleDescription `
     -Access $access -Protocol $protocol -Direction $direction -Priority $priority -SourceAddressPrefix $sourceAddressPrefix `
     -SourcePortRange $sourcePortRange -DestinationAddressPrefix $destinationPortRange -DestinationPortRange $destinationPortRange
 
 
-$nsg | Set-AzureRMNetworkSecurityGroup
+$nsg | Set-AzNetworkSecurityGroup
