96de86c4b807587572d9e8910335c10dbb0f891b
Brad Watts <bwatts@microsoft.com>
2019-05-17 17:09:50 -0400

    Added script to setup backend pool

---
+++ Scripts/set_lbAvailabiitySetBackEndPool.ps1
@@ -0,0 +1,64 @@
+﻿<#
+.SYNOPSIS
+    Updates the Azure Load Balancer backend Pool
+.DESCRIPTION
+    Add's vm's to the backend pool of the specified Azure Load Balancer.
+.OUTPUTS
+    Progress messages
+#>
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $True)]
+    [string]$loadBalancerName,
+    [string]$availabilitySetName,
+    [Parameter(Mandatory = $True)]
+    [string]$backendPoolName
+)
+
+$ErrorActionPreference = "Stop"
+
+Try {
+    $loadBalancer = Get-AzureRmLoadBalancer `
+        -Name $loadBalancerName `
+        -ResourceGroupName $resourceGroupName `
+        -ErrorAction Stop
+}
+Catch {
+    Write-Error "No Load Balancer found with name $loadBalancerName in resource group $resourceGroupName"
+    Return
+}
+
+try {
+    $backendPool = Get-AzureRmLoadBalancerBackendAddressPoolConfig `
+        -Name $backendPoolName `
+        -LoadBalancer $loadBalancer
+}
+catch {
+    #Write-Warning "no Backend Pool found with the name $backendPoolName in the load balancer with the name $loadBalancerName"
+    Return
+}
+
+try {
+    $AvSet = Get-AzureRmAvailabilitySet `
+        -Name $availabilitySetName `
+        -ResourceGroupName (Get-AzureRmResource | Where-Object {
+            ($_.Name -eq $availabilitySetName) -and `
+            ($_.ResourceType -eq "Microsoft.Compute/AvailabilitySets")}).ResourceGroupName
+}
+catch {
+    Write-Error "no AvailabilitySet found with the name $availabilitySetName in resource group $availabilitySetResourceGroup"
+    Return
+}
+
+ForEach ($id in $avSet.VirtualMachinesReferences.id) {
+
+    $nic = Get-AzureRmNetworkInterface | Where-Object {($_.VirtualMachine.id).ToLower() -eq ($id).ToLower()}
+    $nic.IpConfigurations[0].LoadBalancerBackendAddressPools = $backendPool
+
+    Set-AzureRmNetworkInterface -NetworkInterface $nic -AsJob    
+}    
+
+If ($ErrorMessages) {
+    Write-Error "Deployment returned the following errors: $ErrorMessages";
+    Return
+}
