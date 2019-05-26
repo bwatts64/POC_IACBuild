02cc26d156ff5b40c565fb547f654a52328a8e6c
Brad Watts <bwatts@microsoft.com>
2019-05-26 17:38:16 -0400

    Fixed issue with set_lbrule script

--- Scripts/set_lbRule.ps1
+++ Scripts/set_lbRule.ps1
@@ -22,13 +22,15 @@
 )
 
 $slb = Get-AzLoadBalancer -Name $LBName -ResourceGroupName $RGName
+
 $frontEndConfig = Get-AzLoadBalancerFrontendIpConfig -LoadBalancer $slb -Name $frontEndConfigName
+
 $healthProbe = Get-AzLoadBalancerProbeConfig -LoadBalancer $slb -Name $healthProbeName
+
 if($EnableFloatingIP -eq $true) {
-    $slb | Add-AzLoadBalancerRuleConfig -Name $RuleName -Protocol $protocol -LoadDistribution $LoadDistribution -FrontendPort $frontEndPort -BackendPort $backEndPort -FrontendIpConfiguration $frontEndConfig -BackendAddressPool $slb.BackendAddressPools[0] -Probe $healthProbe -EnableFloatingIP
-    Write-Host "A"
+    $slb | Add-AzLoadBalancerRuleConfig -Name $RuleName -Protocol $protocol -FrontendPort $frontEndPort -BackendPort $backEndPort -FrontendIpConfiguration $frontEndConfig -BackendAddressPool $slb.BackendAddressPools[0] -Probe $healthProbe -EnableFloatingIP
 }
 else {
-    $slb | Add-AzLoadBalancerRuleConfig -Name $RuleName -Protocol $protocol -LoadDistribution $LoadDistribution -FrontendPort $frontEndPort -BackendPort $backEndPort -FrontendIpConfiguration $frontEndConfig -BackendAddressPool $slb.BackendAddressPools[0] -Probe $healthProbe
+    $slb | Add-AzLoadBalancerRuleConfig -Name $RuleName -Protocol $protocol -FrontendPort $frontEndPort -BackendPort $backEndPort -FrontendIpConfiguration $frontEndConfig -BackendAddressPool $slb.BackendAddressPools[0] -Probe $healthProbe
 }
 $slb | Set-AzLoadBalancer
